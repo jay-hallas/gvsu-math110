b6d1dcbf188f59caed5fdf0340cd21348a213a69
jay-hallas <hallasjr@gmail.com>
2026-05-28 18:47:33 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,140 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "front-colophon",
+  "level": "1",
+  "url": "front-colophon.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  "
+},
+{
+  "id": "sec-01-01-lin-patterns",
+  "level": "1",
+  "url": "sec-01-01-lin-patterns.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Linear Patterns",
+  "body": " Linear Patterns  This short lesson introduces what it means for a pattern to be linear.    A pattern is linear if for each increase in step by one unit, the number of objects counted in the pattern increases by a constant amount .    Consider the following pattern.   A visual pattern where the number of trees is increasing.   A pattern where at step 1 there are 5 trees, at step 2 there are 7 trees, at step 3 there are 9 trees, and at step 4 there are 11 trees.    Since at each step, the number of trees is increasing by 2, that means the pattern is linear. It is worth noting that with additional information that we might see that the pattern changes. For these questions, we are trying to determine if a pattern fits based on the given data.   Is the Pattern Linear?   Determine whether the following pattern represents linear change or not.   A visual pattern where the number of stars is increasing.   A pattern where at step 1 there is 1 star, at step 2 there are 2 stars, at step 3 there are 4 stars, and at step 4 there are 7 stars.      The pattern is not linear.  Notice at the first step the number of stars increase by 1, but then at the second step the number of stars increase by 2. Since the amount of increase per unit step changes, this cannot be a linear pattern.      Find the length of the hypotenuse of a right triangle with legs of length 5 and 12.    "
+},
+{
+  "id": "def-linear-pattern",
+  "level": "2",
+  "url": "sec-01-01-lin-patterns.html#def-linear-pattern",
+  "type": "Definition",
+  "number": "1.1.1",
+  "title": "",
+  "body": "  A pattern is linear if for each increase in step by one unit, the number of objects counted in the pattern increases by a constant amount .   "
+},
+{
+  "id": "fig-01-01-1-linear-pattern",
+  "level": "2",
+  "url": "sec-01-01-lin-patterns.html#fig-01-01-1-linear-pattern",
+  "type": "Figure",
+  "number": "1.1.2",
+  "title": "",
+  "body": " A visual pattern where the number of trees is increasing.   A pattern where at step 1 there are 5 trees, at step 2 there are 7 trees, at step 3 there are 9 trees, and at step 4 there are 11 trees.   "
+},
+{
+  "id": "sec-01-01-lin-patterns-7",
+  "level": "2",
+  "url": "sec-01-01-lin-patterns.html#sec-01-01-lin-patterns-7",
+  "type": "Example",
+  "number": "1.1.3",
+  "title": "Is the Pattern Linear?",
+  "body": " Is the Pattern Linear?   Determine whether the following pattern represents linear change or not.   A visual pattern where the number of stars is increasing.   A pattern where at step 1 there is 1 star, at step 2 there are 2 stars, at step 3 there are 4 stars, and at step 4 there are 7 stars.      The pattern is not linear.  Notice at the first step the number of stars increase by 1, but then at the second step the number of stars increase by 2. Since the amount of increase per unit step changes, this cannot be a linear pattern.   "
+},
+{
+  "id": "sec-01-01-lin-patterns-8",
+  "level": "2",
+  "url": "sec-01-01-lin-patterns.html#sec-01-01-lin-patterns-8",
+  "type": "Checkpoint",
+  "number": "1.1.5",
+  "title": "",
+  "body": "  Find the length of the hypotenuse of a right triangle with legs of length 5 and 12.   "
+},
+{
+  "id": "sec-practice-quiz-1",
+  "level": "1",
+  "url": "sec-practice-quiz-1.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Practice Quiz 1",
+  "body": " Practice Quiz 1  Only a writing utensil, note card, and a calculator may be used for this quiz. No phones or other web-enabled devices permitted. You must show all work to ensure full credit.     Describe in writing characteristics of linear change.      Consider the linear equation . Fill in the missing values in the table corresponding to this equation.                    For each of the following tables, determine if the change could be linear or not. If the data could be linear, find an equation for the line.                                                                        Find an equation of a line with slope and -intercept .    Using slope-intercept form , we get      Find an equation of a line through and .      For each of the following linear equations, find the and intercepts and sketch the graph.               "
+},
+{
+  "id": "ex-pq1-1",
+  "level": "2",
+  "url": "sec-practice-quiz-1.html#ex-pq1-1",
+  "type": "Exercise",
+  "number": "2.1.1",
+  "title": "",
+  "body": "  Describe in writing characteristics of linear change.   "
+},
+{
+  "id": "ex-pq1-2",
+  "level": "2",
+  "url": "sec-practice-quiz-1.html#ex-pq1-2",
+  "type": "Exercise",
+  "number": "2.1.2",
+  "title": "",
+  "body": "  Consider the linear equation . Fill in the missing values in the table corresponding to this equation.                 "
+},
+{
+  "id": "ex-pq1-3",
+  "level": "2",
+  "url": "sec-practice-quiz-1.html#ex-pq1-3",
+  "type": "Exercise",
+  "number": "2.1.3",
+  "title": "",
+  "body": "  For each of the following tables, determine if the change could be linear or not. If the data could be linear, find an equation for the line.                                                                     "
+},
+{
+  "id": "ex-pq1-4",
+  "level": "2",
+  "url": "sec-practice-quiz-1.html#ex-pq1-4",
+  "type": "Exercise",
+  "number": "2.1.4",
+  "title": "",
+  "body": "  Find an equation of a line with slope and -intercept .    Using slope-intercept form , we get   "
+},
+{
+  "id": "ex-pq1-5",
+  "level": "2",
+  "url": "sec-practice-quiz-1.html#ex-pq1-5",
+  "type": "Exercise",
+  "number": "2.1.5",
+  "title": "",
+  "body": "  Find an equation of a line through and .   "
+},
+{
+  "id": "ex-pq1-6",
+  "level": "2",
+  "url": "sec-practice-quiz-1.html#ex-pq1-6",
+  "type": "Exercise",
+  "number": "2.1.6",
+  "title": "",
+  "body": "  For each of the following linear equations, find the and intercepts and sketch the graph.             "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
